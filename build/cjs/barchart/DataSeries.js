d05c6bee8996f4076942040cc00b6c6a99f7ac16
Vladimir Jelincic <vladimr.jelincic@gmail.com>
2016-11-15 14:27:20 +0100

    Added highlight property to Barchart dataseries

--- build/cjs/barchart/DataSeries.js
+++ build/cjs/barchart/DataSeries.js
@@ -2,7 +2,7 @@
 
 var React = require('react');
 var BarContainer = require('./BarContainer');
-
+var shade = require('../utils').shade;
 module.exports = React.createClass({
 
   displayName: 'DataSeries',
@@ -54,7 +54,7 @@ module.exports = React.createClass({
       width: grouped ? xScale.rangeBand() / series.length : xScale.rangeBand(),
       x: grouped ? xScale(segment.x) + xScale.rangeBand() / series.length * seriesIdx : xScale(segment.x),
       y: segment.y >= 0 ? y : y - barHeight,
-      fill: colors(colorAccessor(segment, seriesIdx)),
+      fill: segment.highlight ? shade(colors(colorAccessor(segment, seriesIdx)) || '#3182BD', 0.5): colors(colorAccessor(segment, seriesIdx)),
       hoverAnimation: hoverAnimation,
       onMouseOver: this.props.onMouseOver,
       onMouseLeave: this.props.onMouseLeave,
